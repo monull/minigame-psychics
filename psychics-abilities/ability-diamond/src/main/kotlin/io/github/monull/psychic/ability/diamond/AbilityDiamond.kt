dbfab41a07006e619843a167bbe6f6eb5bcbe7ec
BuildTools <unconfigured@null.spigotmc.org>
2021-09-20 14:36:46 +0900

    다이아 로드 안 되던 버그 수정

--- psychics-abilities/ability-diamond/src/main/kotlin/io/github/monull/psychic/ability/diamond/AbilityDiamond.kt
+++ psychics-abilities/ability-diamond/src/main/kotlin/io/github/monull/psychic/ability/diamond/AbilityDiamond.kt
@@ -3,8 +3,10 @@ package io.github.monull.psychic.ability.diamond
 import io.github.monull.psychics.Ability
 import io.github.monull.psychics.AbilityConcept
 import io.github.monull.psychics.attribute.EsperAttribute
+import io.github.monull.psychics.attribute.EsperStatistic
 import io.github.monull.psychics.damage.Damage
 import io.github.monull.psychics.damage.DamageType
+import io.github.monull.psychics.tooltip.TooltipBuilder
 import io.github.monull.psychics.util.TargetFilter
 import io.github.monun.tap.config.Config
 import net.kyori.adventure.text.Component
@@ -21,6 +23,7 @@ import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
 
 class AbilityConceptDiamond : AbilityConcept() {
+
     @Config
     var diamondDistance = 40.0
 
@@ -28,27 +31,19 @@ class AbilityConceptDiamond : AbilityConcept() {
         wand = ItemStack(Material.DIAMOND_SWORD)
         damage = Damage.of(DamageType.RANGED, EsperAttribute.ATTACK_DAMAGE to 3.5)
         description = listOf(
-            text("허공에 다이아검 휘두를 시 멀리 있는 상대 공격"),
-            text("최대 사거리 $diamondDistance")
+            text("허공에 다이아검을 휘두를 시 멀리 있는 상대 공격")
+        )
+    }
+
+    override fun onRenderTooltip(tooltip: TooltipBuilder, stats: (EsperStatistic) -> Double) {
+        tooltip.header(
+            text().content("최대 사거리 ").color(NamedTextColor.DARK_RED).decoration(TextDecoration.ITALIC, false)
+                .append(text().content(diamondDistance.toString())).build()
         )
     }
 }
 
-class AbilityDiamond : Ability<AbilityConceptDiamond>(), Listener, Runnable {
-    val rangeTag =
-        text().content("허공에 휘두를 때:").decoration(TextDecoration.ITALIC, false)
-            .color(NamedTextColor.GRAY).build()
-    val diamondRange = text().content("${concept.diamondDistance} 공격 거리").decoration(TextDecoration.ITALIC, false)
-        .color(NamedTextColor.DARK_GREEN).build()
-    val diamondSword = ItemStack(Material.DIAMOND_SWORD).apply {
-        itemMeta = itemMeta.apply {
-            val lore = lore() ?: ArrayList<Component>()
-            lore.add(0, rangeTag)
-            lore.add(1, diamondRange)
-            lore(lore)
-        }
-        durability = 1461
-    }
+class AbilityDiamond : Ability<AbilityConceptDiamond>(), Runnable, Listener {
     override fun onEnable() {
         psychic.registerEvents(this)
         psychic.runTaskTimer(this, 0L, 1L)
@@ -71,12 +66,22 @@ class AbilityDiamond : Ability<AbilityConceptDiamond>(), Listener, Runnable {
     }
 
     override fun run() {
-        esper.player.inventory.run {
-            val iterator = iterator()
-            while (iterator.hasNext()) {
-                var next = iterator.next()
-                if (next.type == Material.DIAMOND_SWORD) {
-                    next = diamondSword
+        if (esper.player.inventory.contains(ItemStack(Material.DIAMOND_SWORD))) {
+            esper.player.inventory.run {
+                val iterator = iterator()
+                while (iterator.hasNext()) {
+                    val next = iterator.next()
+
+                    if (next.type == Material.DIAMOND_SWORD) {
+                        next.itemMeta = next.itemMeta.apply {
+                            val lore = lore() ?: ArrayList<Component>()
+                            lore.add(0, text().content("허공에 휘두를 때: ").decoration(TextDecoration.ITALIC, false)
+                                .color(NamedTextColor.GRAY).build())
+                            lore.add(1, text().content("${concept.diamondDistance} 공격 거리").decoration(TextDecoration.ITALIC, false)
+                                .color(NamedTextColor.DARK_GREEN).build())
+                            lore(lore)
+                        }
+                    }
                 }
             }
         }
